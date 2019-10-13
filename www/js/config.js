9f98db72f632e57d485b46ca8b87d66d04df62a9
pivotmobileapp <jupiter@pivotmobileapp.com>
2019-10-13 14:31:22 -0400

    001

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"https://paymeal.app/driver/api",		
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"Access24@",
+	'debug': false
+};
