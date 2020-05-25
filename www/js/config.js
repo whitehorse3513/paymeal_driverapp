7a5e51e29b3bb1ecdaea5416977e7464b7a3f718
whitehorse3513 <48043357+whitehorse3513@users.noreply.github.com>
2020-05-26 00:55:02 +0300

    Update config.js

--- www/js/config.js
+++ www/js/config.js
@@ -1,7 +1,7 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={			
-	'ApiUrl':"https://paycart.app/driver/api",		
+	'ApiUrl':"https://paymeal.app/driver/api",		
 	'DialogDefaultTitle':"DriverApp",
 	'mapboxToken' : '',
 	'APIHasKey':"Access24@",
